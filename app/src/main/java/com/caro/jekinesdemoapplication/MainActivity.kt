b7c368168c4aa5cabe5e4eda25ee31eeac793953
carozhu <1025807062@qq.com>
2018-11-27 15:06:17 +0800

    init commit

---
+++ app/src/main/java/com/caro/jekinesdemoapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.caro.jekinesdemoapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
